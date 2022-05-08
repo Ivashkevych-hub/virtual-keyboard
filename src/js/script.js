ac0e2e348e266938270e9a7eca28c2f495b3ce10
Alexei Ivashkevych <ivashkevich1991@gmail.com>
2022-05-08 16:21:03 +0300

    feat:complete mouse click on key

--- src/js/script.js
+++ src/js/script.js
@@ -2,43 +2,61 @@ import keyboardBody from './modules/keyboardBody';
 
 keyboardBody();
 
-const keyboard = document.querySelector('.keyboard');
+// const keyboard = document.querySelector('.keyboard');
 const textarea = document.querySelector('.textarea');
 const symbols = document.querySelectorAll('.symbol span');
 const letters = document.querySelectorAll('.letter');
 // let shift = false;
 let caps = false;
-const leftShift = document.querySelector('.left-shift');
-const rightShift = document.querySelector('.right-shift');
-const backspace = document.querySelector('.backspace');
+
 const keys = document.querySelectorAll("[data-code]");
 
-keyboard.addEventListener('click',(e)=>{
-
-  if(e.target.dataset.code == 'Backspace'){
-    textarea.innerHTML = textarea.innerHTML.substring(0,textarea.innerHTML.length-1);
-  }
-  else if(e.target.dataset.code == 'Tab'){
-    textarea.innerHTML += '    ';
-  }
-  else if(e.target.dataset.code == 'Space'){
-    textarea.innerHTML += ' ';
-  }
-  else if(e.target.dataset.code == 'CapsLock'){
-    e.target.classList.toggle('hover') ; 
-    capsLock();
-  }
-  else if (e.target.dataset.code == 'Enter'){
-    textarea.innerHTML += "\n";
-  }
-  else if (e.target.dataset.code == 'Delete'){
-    textarea.innerHTML = textarea.innerHTML.substring(1,textarea.innerHTML.length);
-
-  }else {
-    textarea.innerHTML += e.target.innerText; 
-  }
-
+keys.forEach(key=>{
+    key.addEventListener('click',(e)=>{
+        // console.log(e.target.dataset.code)
+        if(e.target.dataset.code == 'Backspace'){
+          textarea.innerHTML = textarea.innerHTML.substring(0,textarea.innerHTML.length-1);
+        }
+        else if(e.target.dataset.code == 'Tab'){
+          textarea.innerHTML += '    ';
+        }
+        else if(e.target.dataset.code == 'Space'){
+          textarea.innerHTML += ' ';
+        }
+        else if(e.target.dataset.code == 'CapsLock'){
+          e.target.classList.toggle('hover') ; 
+          capsLock();
+        }
+        else if (e.target.dataset.code == 'Enter'){
+          textarea.innerHTML += "\n";
+        }
+        else if (e.target.dataset.code == 'Delete'){
+          textarea.innerHTML = textarea.innerHTML.substring(1,textarea.innerHTML.length);
+      
+        }
+        else if(e.target.dataset.code == 'ShiftRight' ||
+                e.target.dataset.code == 'ShiftLeft' || 
+                e.target.dataset.code == 'ControlLeft' ||
+                e.target.dataset.code == 'ControlRight' || 
+                e.target.dataset.code == 'AltLeft' || 
+                e.target.dataset.code == 'AltRight' ||
+                e.target.dataset.code == 'MetaRight' ||
+                e.target.dataset.code == 'MetaLeft' ||
+                e.target.dataset.code == 'ArrowUp' || 
+                e.target.dataset.code == 'ArrowLeft' ||
+                e.target.dataset.code == 'ArrowRight' ||
+                e.target.dataset.code == 'ArrowDown' 
+                ){
+            textarea.innerHTML += '';
+        }
+        else {
+          textarea.innerHTML += e.target.innerText; 
+        }
+      
+      });
 });
+
+
 
 
 
